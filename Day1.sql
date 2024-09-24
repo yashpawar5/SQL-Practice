0c1408306e89512fd606a14fa4700cf8e2bbc8a9
Yash Pawar <yashpawar.yp5@gmail.com>
2024-09-24 18:39:11 +0530

    1

---
+++ Day1.sql
@@ -0,0 +1,16 @@
+
+SELECT * FROM customer;
+
+SELECT title FROM album;
+
+SELECT artist_id, title FROM album;
+
+SELECT first_name, country, email FROM customer; 
+
+SELECT title FROM album WHERE album_id BETWEEN 200 AND 300;
+
+SELECT * FROM album WHERE title LIKE 'Bongo Fury';
+
+SELECT * FROM customer WHERE city NOT LIKE 'Prague';
+
+SELECT title FROM employee WHERE state NOT IN('A', 'B');

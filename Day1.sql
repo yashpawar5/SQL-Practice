0a2148b0ad9475f885cd21ddd396c16013476c95
Yash Pawar <yashpawar.yp5@gmail.com>
2024-09-26 12:44:40 +0530

    Few new queries added.

--- Day1.sql
+++ Day1.sql
@@ -1,17 +1,35 @@
-SELECT * FROM customer;
+SELECT *
+FROM customer;
 
-SELECT title FROM album;
+SELECT title
+FROM album;
 
-SELECT artist_id, title FROM album;
+SELECT artist_id,
+    title
+FROM album;
 
-SELECT first_name, country, email FROM customer; 
+SELECT first_name,
+    country,
+    email
+FROM customer;
 
-SELECT title FROM album WHERE album_id BETWEEN 200 AND 300;
+SELECT title
+FROM album
+WHERE album_id BETWEEN 200 AND 300;
 
-SELECT * FROM album WHERE title LIKE 'Bongo Fury';
+SELECT *
+FROM album
+WHERE title LIKE 'Bongo Fury';
 
-SELECT * FROM customer WHERE city NOT LIKE 'Prague';
+SELECT *
+FROM customer
+WHERE city NOT LIKE 'Prague';
 
-SELECT title FROM employee WHERE state NOT IN('A', 'B');
+SELECT title
+FROM employee
+WHERE state NOT IN('A', 'B');
 
-SELECT first_name FROM customer WHERE first_name LIKE 'J%' AND LENGTH(first_name) BETWEEN 3 AND 5;
+SELECT first_name
+FROM customer
+WHERE first_name LIKE 'J%'
+    AND LENGTH(first_name) BETWEEN 3 AND 5;
